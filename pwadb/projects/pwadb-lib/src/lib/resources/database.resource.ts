d17ae8f18e2eda9121afc549771017e475a0cf0c
Priyanshu Bhatnagar <priyanshu@advancedware.in>
2020-07-07 13:41:00 +0530

    Updated unsynchronised function in database service

--- pwadb/projects/pwadb-lib/src/lib/resources/database.resource.ts
+++ pwadb/projects/pwadb-lib/src/lib/resources/database.resource.ts
@@ -61,9 +61,7 @@ export class PwaDatabaseService<T> {
 
         return this.db$.pipe(
 
-            tap(db => console.log(db, collectionNames)),
-
-            map(db => collectionNames.filter(k => k in db).map(k => ({collectionName: k, documents$: (db[k] as PwaCollection<any>).find({selector: {$and: [{tenant: {$eq: tenant}}, {method: {$ne: 'GET'}}]}, sort: [{time: order}]}).$}))),
+            map(db => collectionNames.filter(k => k in db.collections).map(k => ({collectionName: k, documents$: (db.collections[k] as PwaCollection<any>).find({selector: {$and: [{tenant: {$eq: tenant}}, {method: {$ne: 'GET'}}]}, sort: [{time: order}]}).$}))),
 
             switchMap(v => combineLatest(v.map(x => x.documents$.pipe(map(docs => docs.map(d => ({collectionName: x.collectionName, document: d}))))))),
 
