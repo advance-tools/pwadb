c98750debaef145bbcd0a7542f6145ad6de72250
Priyanshu Bhatnagar <priyanshu@advancedware.in>
2020-07-19 01:38:26 +0530

    PwaCollectionAPI updated

--- pwadb/projects/pwadb-lib/src/lib/resources/collection.resource.ts
+++ pwadb/projects/pwadb-lib/src/lib/resources/collection.resource.ts
@@ -62,7 +62,7 @@ export class CollectionAPI<T extends Datatype, Database> {
 
     makeTenantUrl(tenant: string, url: string): string {
 
-        return `${tenant}-$-${url}`;
+        return `${tenant}----${url}`;
     }
 
     filterList(docs$: Observable<PwaDocument<T>[]>, params?: HttpParams, validQueryKeys = []): Observable<CollectionListResponse<T>> {
