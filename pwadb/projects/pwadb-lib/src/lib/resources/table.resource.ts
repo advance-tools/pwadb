6636f3791507cc0d63e2dc1bc2a357c18a73a4e0
Priyanshu Bhatnagar <priyanshu@advancedware.in>
2021-05-04 17:11:13 +0000

    debugging frontendCursor pagination

--- pwadb/projects/pwadb-lib/src/lib/resources/table.resource.ts
+++ pwadb/projects/pwadb-lib/src/lib/resources/table.resource.ts
@@ -100,6 +100,8 @@ export class BaseDatabase<T extends TableDataType> implements IBaseDatabase {
             }
         }
 
+        console.log('loadmore httpParams', this.httpParams.toString);
+
         this._httpParams = this.httpParams.set('limit', this.limit.toString());
 
         if (!this.httpParams.has('ordering')) { this._httpParams = this.httpParams.set('ordering', '-created_at'); }
