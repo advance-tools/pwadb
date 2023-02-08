46ec9d4df0967e5d778ab456093076ebdb042919
impriyanshub <priyanshu@advancedware.in>
2023-02-08 19:55:20 +0000

    websocketWithoutId class added

--- pwadb-nx/libs/pwadb-lib/src/lib/resources/websocket.resource.ts
+++ pwadb-nx/libs/pwadb-lib/src/lib/resources/websocket.resource.ts
@@ -84,3 +84,69 @@ export class SocketOperation<T extends Datatype, Database> {
         this.subs.unsubscribe();
     }
 }
+
+//////////////////////////
+// Socket Op Without ID
+//////////////////////////
+export interface WebsocketWithoutIdService<T extends Datatype, Database> extends PwaCollectionAPI<T, Database> {
+    retrieve: (params?: HttpParams) => Observable<PwaDocument<any>>,
+    retrieveReactive: (params?: HttpParams) => Observable<PwaDocument<any>>,
+}
+
+export class SocketOperationWithoutId<T extends Datatype, Database> {
+
+    private subs = new Subscription();
+
+    constructor(
+        public entity: string,
+        public apiService: WebsocketWithoutIdService<T, Database>,
+        public websocketNotificationService: WebsocketNotificationService
+    ) {
+
+        const subs = websocketNotificationService.getEntityMessage(entity).pipe(
+
+            // emit distinct output when record_id and operation changes
+            distinctUntilChanged((prev, cur) => prev.record_id === cur.record_id && prev.operation === cur.operation),
+
+            switchMap(v => {
+
+                return apiService.collectionAPI.collection$.pipe(
+
+                    // find the data in collection
+                    switchMap(col => col.findOne({selector: {id: {eq: v.record_id}}}).exec()),
+
+                    // filter out emit if data is not present
+                    filter(doc => !!doc),
+
+                    switchMap(doc => {
+
+                        console.log('filtered emit', v, doc.toMutableJSON().data)
+
+                        switch (v.operation) {
+
+                            case 'DELETE': {
+
+                                // remove document if exists
+                                return doc.remove();
+                            }
+
+                            default: {
+
+                                // retrieve data for latest changes
+                                return apiService.retrieve();
+                            }
+                        }
+                    })
+                );
+            }),
+
+        ).subscribe();
+
+        this.subs.add(subs);
+    }
+
+    unsubscribe() {
+
+        this.subs.unsubscribe();
+    }
+}
